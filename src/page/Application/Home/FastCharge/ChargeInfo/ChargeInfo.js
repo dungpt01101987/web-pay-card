74c707f1e77c7559a41e8902130b5a89289a4c65
PhamTienDung <phamdung110@gmail.com>
2024-01-19 14:15:36 +0700

    update

--- src/page/Application/Home/FastCharge/ChargeInfo/ChargeInfo.js
+++ src/page/Application/Home/FastCharge/ChargeInfo/ChargeInfo.js
@@ -39,43 +39,66 @@ const listProductSpecification = [
 const listProductDenominationChina = [
     {
         denominationId: 1,
-        denomination: '100 yuan'
+        denomination: 100,
+        type: "yuan"
     },
     {
         denominationId: 2,
-        denomination: '200 yuan'
+        denomination: 200,
+        type: "yuan"
     },
     {
         denominationId: 3,
-        denomination: '200 yuan'
+        denomination: 300,
+        type: "yuan"
     },
 ];
 const listProductDenominationViet = [
     {
         denominationId: 1,
-        denomination: '1000 VND'
+        denomination: 100,
+        type: "VNĐ"
     },
     {
         denominationId: 2,
-        denomination: '1000 VND'
+        denomination: 200,
+        type: "VNĐ"
     },
     {
         denominationId: 3,
-        denomination: '1000 VND'
+        denomination: 300,
+        type: "VNĐ"
     },
 ]
 
 
 const ChargeInfo = ({ title }) => {
-    const [selectedProductSpecification, setSelectedProductSpecification] = useState(null);
-    const [selectedProductDenomination, setselectedProductDenomination] = useState(null);
+    const [selectedProductSpecification, setSelectedProductSpecification] = useState(1);
+    const [selectedProductDenomination, setselectedProductDenomination] = useState(
+        {
+            denominationId: 1,
+            denomination: 100,
+            type: "yuan"
+        }
+    );
+    const [value, setValue] = useState(1);
 
     const handleIconClick = (productId) => {
         setSelectedProductSpecification(productId);
     };
 
-    const handleIconClickDenomination = (denominationId) => {
-        setselectedProductDenomination(denominationId);
+    const handleIconClickDenomination = (denomination) => {
+        setselectedProductDenomination(denomination);
+    };
+
+
+
+    const handleIncrease = () => {
+        setValue(prevValue => prevValue + 1);
+    };
+
+    const handleDecrease = () => {
+        setValue(prevValue => prevValue - 1);
     };
 
     return (
@@ -111,8 +134,8 @@ const ChargeInfo = ({ title }) => {
                     {selectedProductSpecification && selectedProductSpecification === 1 && listProductDenominationChina.map((denomiation) => (
                         <div
                             key={denomiation.id}
-                            onClick={() => handleIconClickDenomination(denomiation.id)}
-                            className={cx('info-options__item', { active: denomiation.id === selectedProductDenomination })}
+                            onClick={() => handleIconClickDenomination(denomiation)}
+                            className={cx('info-options__item', { active: denomiation.denominationId === selectedProductDenomination.denominationId })}
                         >
                             {denomiation.denomination}
                         </div>
@@ -120,8 +143,8 @@ const ChargeInfo = ({ title }) => {
                     {selectedProductSpecification && selectedProductSpecification === 2 && listProductDenominationViet.map((denomiation) => (
                         <div
                             key={denomiation.id}
-                            onClick={() => handleIconClickDenomination(denomiation.id)}
-                            className={cx('info-options__item', { active: denomiation.id === selectedProductDenomination })}
+                            onClick={() => handleIconClickDenomination(denomiation)}
+                            className={cx('info-options__item', { active: denomiation.denominationId === selectedProductDenomination.denominationId })}
                         >
                             {denomiation.denomination}
                         </div>
@@ -129,7 +152,32 @@ const ChargeInfo = ({ title }) => {
                 </div>
             </div>
             <div className={cx('info-num')}>
+                Quantity:
+                <div className={cx('info-num__cmp')}>
+                    <button
+                        onClick={handleDecrease}
+                        className={cx('el-input-number__decrease', { disabled: value <= 1 })}
+                    >
+                        <i className={cx('el-icon')}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path fill="currentColor" d="M128 544h768a32 32 0 1 0 0-64H128a32 32 0 0 0 0 64z"></path></svg>
+                        </i>
+                    </button>
+                    <button onClick={handleIncrease} className={cx('el-input-number__increase')}>
+                        <i className={cx('el-icon')}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path fill="currentColor" d="M480 480V128a32 32 0 0 1 64 0v352h352a32 32 0 1 1 0 64H544v352a32 32 0 1 1-64 0V544H128a32 32 0 0 1 0-64h352z"></path></svg>
+                        </i>
+                    </button>
+                    <input type="text" value={value} readOnly className={cx('el-input')} />
+                </div>
+            </div>
 
+            <div className={cx('info-footer')}>
+                <h5 className={cx('info-footer__title')}>Total Price</h5>
+                <div className={cx('info-footer__wrap')}>
+                    <span className={cx('info-footer__price')}>{selectedProductDenomination.denomination * value} ({selectedProductDenomination.type}) </span>
+                    <button className={cx('info-footer__btn')}>Buy Now</button>
+                </div>
+                <p className={cx('info-footer__sub')}>98% of Orders Guaranteed Delivered within one minute Completed!</p>
             </div>
         </div >
     );
